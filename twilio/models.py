540ab836fafd0459890149f78cd7230981e92269
Muhammad Alhajee <alhajee2009@gmail.com>
2017-09-18 05:34:03 +0100

    testing Inbox date time

--- twilio/models.py
+++ twilio/models.py
@@ -6,3 +6,8 @@ class Inbox(models.Model):
     sender = models.CharField(max_length=50)
     receive_date = models.DateTimeField('date received')
 
+    def __str__(self):
+        return self.message_text + "-" + \
+               self.sender + "-" + \
+               self.receive_date.strftime('%Y-%m-%d %H:%M')
+
