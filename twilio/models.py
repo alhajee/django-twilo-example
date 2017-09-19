db07c476a6a88048052e5f98cbcc5d30a82e7a8c
Muhammad Alhajee <alhajee2009@gmail.com>
2017-09-19 20:53:31 +0100

    Added django admin database access

--- twilio/models.py
+++ twilio/models.py
@@ -1,5 +1,7 @@
-from django.db import models
+import datetime
 
+from django.db import models
+from django.utils import timezone
 
 class Inbox(models.Model):
     message_text = models.CharField(max_length=640)
@@ -7,7 +9,10 @@ class Inbox(models.Model):
     receive_date = models.DateTimeField('date received')
 
     def __str__(self):
-        return self.message_text + "-" + \
-               self.sender + "-" + \
+        return self.message_text + ", " + \
+               self.sender + ", " + \
                self.receive_date.strftime('%Y-%m-%d %H:%M')
+
+    def was_received_recently(self):
+        return self.receive_date >= timezone.now() - datetime.timedelta(days=1)
 
